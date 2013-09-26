770dfdd6909ddf62914091cae51efce0f6c8bf47
NeilvB <naldenvb@gmail.com>
2013-09-26 22:40:42 +0100

    Clean up a couple of for loops

--- glasto.js
+++ glasto.js
@@ -3,33 +3,31 @@ var glastoModule = (function () {
 
     /**
      * Loads user details from local storage into text fields.
-     */ 
+     */
     function loadPeople() {
-        var people = JSON.parse(localStorage.getItem("people")) || [],
-            i = 0;
-        
-        for(;i < people.length; i += 1) {            
+        var people = JSON.parse(localStorage.getItem("people")) || [];
+
+        for (var i = 0, j = people.length; i < j; i += 1) {
             document.getElementById("registrationNo" + i).value = people[i].registrationNumber;
             document.getElementById("postcode" + i).value = people[i].postcode;
-        }    
+        }
     }
-    
+
     /**
      * Saves user details from text fields into local storage.
-     */ 
+     */
     function savePeople() {
-        var people = [],
-            i = 0;
-        
-        for(;i < 6; i += 1) {
+        var people = [];
+
+        for (i = 0; i < 6; i += 1) {
             people.push({
                 registrationNumber : document.getElementById("registrationNo" + i).value,
                 postcode : document.getElementById("postcode" + i).value
             });
-        }                                    
+        }
         localStorage.setItem("people", JSON.stringify(people));
     }
-    
-    loadPeople();                                
+
+    loadPeople();
     document.getElementById("save").onclick = savePeople;
 }());
