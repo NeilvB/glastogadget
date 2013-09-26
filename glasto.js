87fee001d39bdf04be6d38f67923d395375f58cc
NeilvB <naldenvb@gmail.com>
2013-09-26 22:07:57 +0100

    Add glasto JS file wrapped in module

---
+++ glasto.js
@@ -0,0 +1,22 @@
+var glastoModule = (function () {
+    var people = JSON.parse(localStorage.getItem("people")) || [],
+        i = 0;
+            
+    for(;i < people.length; i += 1) {            
+        document.getElementById("registrationNo" + i).value = people[i].registrationNumber;
+        document.getElementById("postcode" + i).value = people[i].postcode;
+    }                    
+
+    document.getElementById("save").onclick = function() {
+        var people = [],
+            i = 0;
+        
+        for(;i < 6; i += 1) {
+            people.push({
+                registrationNumber : document.getElementById("registrationNo" + i).value,
+                postcode : document.getElementById("postcode" + i).value
+            });
+        }                                    
+        localStorage.setItem("people", JSON.stringify(people));
+    }
+}());
