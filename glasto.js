df0181badbcf0ab6a16bfe7e8a72b31b7d544caa
NeilvB <naldenvb@gmail.com>
2013-09-26 22:22:31 +0100

    Extract a couple of methods and commenting

--- glasto.js
+++ glasto.js
@@ -1,15 +1,23 @@
 var glastoModule = (function () {
     'use strict';
-    
-    var people = JSON.parse(localStorage.getItem("people")) || [],
-        i = 0;
-            
-    for(;i < people.length; i += 1) {            
-        document.getElementById("registrationNo" + i).value = people[i].registrationNumber;
-        document.getElementById("postcode" + i).value = people[i].postcode;
-    }                    
 
-    document.getElementById("save").onclick = function() {
+    /**
+     * Loads user details from local storage into text fields.
+     */ 
+    function loadPeople() {
+        var people = JSON.parse(localStorage.getItem("people")) || [],
+            i = 0;
+        
+        for(;i < people.length; i += 1) {            
+            document.getElementById("registrationNo" + i).value = people[i].registrationNumber;
+            document.getElementById("postcode" + i).value = people[i].postcode;
+        }    
+    }
+    
+    /**
+     * Saves user details from text fields into local storage.
+     */ 
+    function savePeople() {
         var people = [],
             i = 0;
         
@@ -20,5 +28,8 @@ var glastoModule = (function () {
             });
         }                                    
         localStorage.setItem("people", JSON.stringify(people));
-    };
+    }
+    
+    loadPeople();                                
+    document.getElementById("save").onclick = savePeople;
 }());
